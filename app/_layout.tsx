011658a6a6d0ab4e5819cd26249363bd7b787bdd
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-03-27 17:10:02 +0530

    added safe area provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,11 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar hidden={true} />
-
       <Stack>
         <Stack.Screen
           name="(tabs)"
@@ -14,6 +14,6 @@ export default function RootLayout() {
           }}
         />
       </Stack>
-  </>
-  )
+    </SafeAreaProvider>
+  );
 }
